1d9f4fe47c355b89a2ccd0407f024246c37774c6
mhd-1999 <maihaiduong2015@gmail.com>
2022-08-23 10:08:57 +0700

    e0002-days2

--- src/components/CompUploadFile.vue
+++ src/components/CompUploadFile.vue
@@ -1,23 +1,24 @@
 <template>
   <div
     id="drop-zone"
-    @dragenter="OnDragEnter"
-    @dragleave="OnDragLeave"
+    @dragenter.prevent="toggleActive"
+    @dragleave.prevent="toggleActive"
     @dragover.prevent
-    :class="{ dragging: isDragging }"
+    @drop.prevent="drop"
+    :class="{'active-dropzone' : active }"
   >
     <div class="upload-logo">
       <img src="../assets/img/upload.png" alt="" />
     </div>
     <div class="upload-content">
       <h3>Drag and drop files</h3>
-      <form @submit.prevent="handleSubmit">
+      <form >
         <input
           type="file"
-          @change="handleChangeFiles"
           name="uploadFileName"
           id="uploadFile"
           class="uploadFile"
+          @change="handleChangeFiles"
         />
         <label for="uploadFile">Browse files</label>
       </form>
@@ -27,29 +28,46 @@
 </template>
 
 <script>
+import {ref} from "vue"
+
 export default {
   name: "DropZone",
   data() {
     return {
-      isDragging: false,
       files: [],
     };
   },
+  setup(){
+    let active=ref(false);
+    //  let dropzoneFile=ref("");
+    // let drop=(e)=>{
+    //   dropzoneFile.value=e.dataTranfer.files[0];
+    //   this.files.push(dropzoneFile.value);
+    // }
+    let toggleActive=()=>{
+      active.value = !active.value;
+    };
+    return {active,toggleActive};
+  },
   methods: {
-    OnDragEnter(e) {
-      e.preventDefault();
-      this.isDragging = true;
-    },
-    OnDragLeave(e) {
-      e.preventDefault();
-      this.isDragging = false;
-    },
-    handleSubmit() {
-      let data = new FormData();
-      data.append("files[]", this.files);
-    },
+    // OnDragEnter(e) {
+    //   e.preventDefault();
+    //   this.isDragging = true;
+    // },
+    // OnDragLeave(e) {
+    //   e.preventDefault();
+    //   this.isDragging = false;
+    // },
+    // handleSubmit() {
+    //   let data = new FormData();
+    //   data.append("files[]", this.files);
+    //   const storageRef=stRef(storage,"files/"+file.name);
+    //   uploadBytes(storageRef,data).then((snapshot){
+    //     var newFileRef=push()
+    //   })
+    // },
     handleChangeFiles(e) {
-      let file = e.target.files[0];
+      let file = e.dataTranfer.files[0];
       this.files.push(file);
     },
   },
@@ -66,9 +84,10 @@ export default {
   border: 1px solid #dcdcdc;
   padding-bottom: 60px;
 }
-.dragging {
+.active-dropzone {
   opacity: 0.5;
   border: 1px dashed #000000 !important;
+  background: chocolate !important;
 }
 .upload-logo {
   width: 100%;
