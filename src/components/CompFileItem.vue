999515c7aac706b8c5d655fba37c983c49b12d70
mhd-1999 <maihaiduong2015@gmail.com>
2022-08-25 09:04:40 +0700

    last-update

--- src/components/CompFileItem.vue
+++ src/components/CompFileItem.vue
@@ -1,16 +1,28 @@
 <template>
-        <div class="file-item">
-            <img :src="docIcon" v-if="fileType=='application/vnd.openxmlformats-officedocument.wordprocessingml.document'"/>
-            <img :src="pdfIcon" v-else-if="fileType=='application/pdf'"/>
-            <img :src="excelIcon" v-else-if="fileType=='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'"/>
-            <img :src="defaultIcon" v-else/>
-            <div class="file-text">
-                <p>{{fileName}}</p>
-                <span>{{fileSize}}kB</span>
-            </div>
-            
-            <slot></slot>
-        </div>
+  <div class="file-item">
+    <img
+      :src="docIcon"
+      v-if="
+        fileType ==
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+      "
+    />
+    <img :src="pdfIcon" v-else-if="fileType == 'application/pdf'" />
+    <img
+      :src="excelIcon"
+      v-else-if="
+        fileType ==
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      "
+    />
+    <img :src="defaultIcon" v-else />
+    <div class="file-text">
+      <p>{{ fileName }}</p>
+      <span>{{ fileSize }}kB</span>
+    </div>
+
+    <slot></slot>
+  </div>
 </template>
 
 <script>
@@ -19,86 +31,85 @@ import excelIcon from "../assets/img/excel.png";
 import pdfIcon from "../assets/img/pdf.png";
 import defaultIcon from "../assets/img/default.png";
 export default {
-    name:'FileItem',
-    data(){
-        return{
-            docIcon:docIcon,
-            excelIcon:excelIcon,
-            pdfIcon:pdfIcon,
-            defaultIcon:defaultIcon,
-        }
+  name: "FileItem",
+  data() {
+    return {
+      docIcon: docIcon,
+      excelIcon: excelIcon,
+      pdfIcon: pdfIcon,
+      defaultIcon: defaultIcon,
+    };
+  },
+  method: {
+    handelDelete() {
+      this.$emit("handleDeleteFile");
     },
-    method:{
-        handelDelete(){
-            this.$emit('handleDeleteFile')
-        }
+  },
+  props: {
+    fileType: {
+      type: String,
+      required: true,
     },
-    props:{
-        fileType:{
-            type: String,
-            required: true,
-        },
-        fileName:{
-            type: String,
-            required: true,
-        },
-        fileSize:{
-            type: Number,
-            required: true,
-        },
-        
+    fileName: {
+      type: String,
+      required: true,
     },
-    // computed:{
-    //     typeFile(){
-    //         return this.fileType;
-    //     }
-    // }
-}
+    fileSize: {
+      type: Number,
+      required: true,
+    },
+  },
+  // computed:{
+  //     typeFile(){
+  //         return this.fileType;
+  //     }
+  // }
+};
 </script>
 
 <style scoped>
-    p{
-        margin:0;
-        font-size: 12px;
-        font-weight: 700;
-        line-height: 16px;
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    }
-    span{
-        font-weight: 400;
-        font-size: 10px;
-        line-height: 14px;
-        color: #666666;
-    }
-    img{
-        width: 32px;
-        height: 32px;
-        padding:8px 12px;
-    }
-    .file-item{
-        display: flex;
-        background: #FFFFFF;
-        border: 1px solid #DCDCDC;
-        border-radius: 3px;
-        width: 244px;
-        justify-content: center;
-        margin-right:17px;
-    }
-    .file-item .type-icon{
-        padding: 8px 12px;
-        width: 32px;
-        height: 32px;
-    }
-    .file-text{
-        width: 100%;
-        overflow: hidden;
-        padding-right: 12px;
-        white-space: nowrap;
-        padding: 8px ;
-    }
-    .file-item .close-icon{
-        width: 16px;
-        height: 16px;
-        padding: 16px 8px;
-    }
+p {
+  margin: 0;
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 16px;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+}
+span {
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 14px;
+  color: #666666;
+}
+img {
+  width: 32px;
+  height: 32px;
+  padding: 8px 12px;
+}
+.file-item {
+  display: flex;
+  background: #ffffff;
+  border: 1px solid #dcdcdc;
+  border-radius: 3px;
+  width: 244px;
+  justify-content: center;
+  margin-right: 17px;
+}
+.file-item .type-icon {
+  padding: 8px 12px;
+  width: 32px;
+  height: 32px;
+}
+.file-text {
+  width: 100%;
+  overflow: hidden;
+  padding-right: 12px;
+  white-space: nowrap;
+  padding: 8px;
+}
+.file-item .close-icon {
+  width: 16px;
+  height: 16px;
+  padding: 16px 8px;
+}
 </style>
--- src/components/CompUploadFile.vue
+++ src/components/CompUploadFile.vue
@@ -1,123 +1,124 @@
 <template>
-<div class="upload">
-<div
-    id="drop-zone"
-    @dragenter="isActive=true"
-    @mouseleave="isActive=false"
-    @drop.prevent="handleDragFiles"
-    @dragover.prevent
-    :class="{'active-dropzone' : isActive ,'active-err': isOver}"
-  >
-    <div class="upload-logo" @click="handleUpload">
-      <img src="../assets/img/upload.png" alt="" />
+  <div class="upload">
+    <div
+      id="drop-zone"
+      @dragenter="isActive = true"
+      @mouseleave="isActive = false"
+      @drop.prevent="handleDragFiles"
+      @dragover.prevent
+      :class="{ 'active-dropzone': isActive, 'active-err': isOver }"
+    >
+      <div class="upload-logo" @click="handleUpload">
+        <img src="../assets/img/upload.png" alt="" />
+      </div>
+      <div class="upload-content">
+        <h3>Drag and drop files</h3>
+        <form>
+          <input
+            type="file"
+            name="uploadFileName"
+            id="uploadFile"
+            class="uploadFile"
+            @change="handleChangeFiles"
+          />
+          <label for="uploadFile">Browse files</label>
+        </form>
+      </div>
     </div>
-    <div class="upload-content">
-      <h3>Drag and drop files</h3>
-      <form >
-        <input
-          type="file"
-          name="uploadFileName"
-          id="uploadFile"
-          class="uploadFile"
-          @change="handleChangeFiles"
+    <p class="err-mess" v-show="isOver">The maximum file size is 10 MB</p>
+    <div class="file-list">
+      <file-item
+        v-for="(file, index) in data"
+        :key="index"
+        :fileName="file.name"
+        :fileSize="parseInt(file.size / 1024)"
+        :fileType="file.type"
+        ref="fileItem"
+      >
+        <img
+          class="close-icon"
+          :src="closeIcon"
+          alt=""
+          @click="handleDelete(index)"
         />
-        <label for="uploadFile">Browse files</label>
-      </form>
+      </file-item>
     </div>
-</div>
-<p class="err-mess" v-show="isOver">The maximum file size  is 10 MB</p>
-<!-- <p v-for="(file, index) in data" :key="index">{{ file.name }}||{{file.type}}||{{file.size}}</p> -->
-<div class="file-list">
-  
-  <file-item  
-    v-for="(file,index) in data" 
-    :key="index" 
-    :fileName="file.name" 
-    :fileSize="parseInt((file.size/1024))"
-    :fileType="file.type">
-
-    <img class="close-icon" 
-         :src="closeIcon" alt="" 
-         @click="handleDelete(index)">
-  </file-item>
-</div>
   </div>
 </template>
 
 <script>
-
-import {ref,uploadBytes } from 'firebase/storage';
-import {storage} from '../firebase.config';
-import FileItem from './CompFileItem.vue';
-import excelIcon from '../assets/img/excel.png';
-import closeIcon from '../assets/img/close-circle.png';
+import { ref, uploadBytes } from "firebase/storage";
+import { storage } from "../firebase.config";
+import FileItem from "./CompFileItem.vue";
+import excelIcon from "../assets/img/excel.png";
+import closeIcon from "../assets/img/close-circle.png";
 export default {
   name: "DropZone",
-  components:{
-    FileItem:FileItem,
+  components: {
+    FileItem: FileItem,
   },
   data() {
     return {
       data: [],
-      isNull:false,
-      isOver:false,
+      isNull: false,
+      isOver: false,
       isActive: false,
-      excelIcon:excelIcon,
-      closeIcon:closeIcon,
+      excelIcon: excelIcon,
+      closeIcon: closeIcon,
+      currentIdx: 0,
+      left: 0,
     };
   },
-
+  computed: {},
   methods: {
-    //check theChange 
+    //check theChange
     handleChangeFiles(e) {
-      let files=e.target.files[0];
-      if(files.size>10000000){
-        this.isOver=true;
-      }else{
+      let files = e.target.files[0];
+      if (files.size > 10000000) {
+        this.isOver = true;
+      } else {
         this.data.push(files);
-        this.isOver=false;
+        this.isOver = false;
         console.log(files.type);
       }
-      // 
+      //
       // console.log(this.data);
     },
 
     handleDragFiles(e) {
-      let files=e.dataTransfer.files[0];
-      if(files.size>1000000){
-        this.isOver=true;
-      }else{
+      let files = e.dataTransfer.files[0];
+      if (files.size > 1000000) {
+        this.isOver = true;
+      } else {
         this.data.push(files);
-        this.isOver=false;
+        this.isOver = false;
       }
     },
 
-    handleUpload(){
-      this.data.forEach((file)=>{
-       const fileRef=ref(storage,`${file.name}`);
-       uploadBytes(fileRef,file).then(()=>{
-        this.data.forEach((file,index)=>{
-          //Remove item in data array when upload to firebase
-          if(file.name==file.name){
-            this.data.splice(index,1);
-          }
-        })
-       })
-      })
+    handleUpload() {
+      this.data.forEach((file) => {
+        const fileRef = ref(storage, `${file.name}`);
+        uploadBytes(fileRef, file).then(() => {
+          this.data.forEach((file, index) => {
+            //Remove item in data array when upload to firebase
+            if (file.name == file.name) {
+              this.data.splice(index, 1);
+            }
+          });
+        });
+      });
     },
 
-    handleDelete(index){
-     this.data.splice(index,1);
+    handleDelete(index) {
+      this.data.splice(index, 1);
     },
-
   },
-
 };
 </script>
 
 <style scoped>
-.upload{
-  margin:100px auto;
+.upload {
+  margin: 100px auto;
   width: 842px;
 }
 #drop-zone {
@@ -130,21 +131,21 @@ export default {
   border-radius: 7px;
   padding-bottom: 60px;
 }
-h3{
-  margin-bottom:0;
-  margin-top:25px;
+h3 {
+  margin-bottom: 0;
+  margin-top: 25px;
 }
 .active-dropzone {
   opacity: 0.5;
   border: 1px dashed #000000 !important;
 }
-.active-err{
-  border:1px solid red !important;
+.active-err {
+  border: 1px solid red !important;
 }
-.err-mess{
+.err-mess {
   width: 100%;
-  padding-top:7px;
-  margin:0;
+  padding-top: 7px;
+  margin: 0;
   text-align: start;
   color: red;
 }
@@ -152,7 +153,6 @@ h3{
   width: 100%;
   padding-top: 60px;
   text-align: center;
-
 }
 .upload-logo img {
   width: 32px;
@@ -169,8 +169,11 @@ h3{
 .upload-content label {
   text-decoration: underline;
 }
-.file-list{
+.file-list {
   display: flex;
-  margin-top: 33px;
+  width: 842px;
+  margin: 33px auto;
+  padding-bottom: 33px;
+  overflow-x: scroll;
 }
 </style>
